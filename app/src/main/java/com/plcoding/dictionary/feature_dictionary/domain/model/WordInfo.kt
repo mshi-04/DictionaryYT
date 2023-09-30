dce0f14741451287d3dc7ddf9cc68d46276dca96
masato <masato93426@gmail.com>
2023-09-30 16:30:56 +0900

    init

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/domain/model/WordInfo.kt
@@ -0,0 +1,8 @@
+package com.plcoding.dictionary.feature_dictionary.domain.model
+
+data class WordInfo(
+    val meanings: List<Meaning>,
+    val phonetic: String,
+    val origin: String,
+    val word: String
+)
